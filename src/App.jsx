bd76d8e8d45266999145824d3e69eb861ff65514
Gianfranco <gianfranco.devita@hotmail.com>
2026-03-21 11:06:19 +0100

    feat: Install dependencies and implement them

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 import "./App.css";
 
 function App() {
 	return (
 		<>
 			<h1>Nexo-Project</h1>
+
+			<Routes>
+				<Route path={"/"} element={<HomePage />} />
+			</Routes>
 		</>
 	);
 }
