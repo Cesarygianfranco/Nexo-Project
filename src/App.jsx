76f3ed7c875a6d48469f9073de8b41bd2de9c12c
Cesar <cessardevs@gmail.com>
2026-03-21 10:16:34 +0000

    feat: implement Mantine setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
 
 function App() {
-	return (
-		<>
-			<h1>Nexo-Project</h1>
+  return (
+    <>
+      <MantineProvider>
+        <h1>Nexo-Project</h1>
 
-			<Routes>
-				<Route path={"/"} element={<HomePage />} />
-			</Routes>
-		</>
-	);
+        <Routes>
+          <Route path={"/"} element={<HomePage />} />
+        </Routes>
+
+      </MantineProvider>
+    </>
+  );
 }
 
 export default App;
