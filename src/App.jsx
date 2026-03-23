c5cb62ac7fb192e0d5bd5f7c835564ac5d0f998e
Cesar <cessardevs@gmail.com>
2026-03-23 10:39:47 +0000

    feat: Implement Header desing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import HomePage from "./pages/HomePage";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import "./App.css";
+import HeaderSearch from "./components/HeaderSearch"
 
 function App() {
   return (
     <>
       <MantineProvider>
-        <h1>Nexo-Project</h1>
+        
+        <HeaderSearch />
 
         <Routes>
           <Route path={"/"} element={<HomePage />} />
