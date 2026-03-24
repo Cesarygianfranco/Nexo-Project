176cb3c8164d84baf43b12fdf810f196b57fc545
Cesar <cessardevs@gmail.com>
2026-03-24 15:25:44 +0000

    feat: changed colors for card and navbar

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,34 +1,37 @@
 import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+import { Link } from "react-router-dom"
 
 const CategoryCard = (props) => {
   return (
     <div>
       <Card shadow="sm" padding="lg" radius="md" withBorder maw={300}>
         <Card.Section>
+          <Link to={`/products/${props.categoryObj.id}`}>
           <Image
             src={props.categoryObj.icon}
             height={160}
             miw={300} 
             alt="Category Image"
           />
+          </Link>
         </Card.Section>
 
         <Group justify="space-between" mt="md" mb="xs">
           <Text fw={500}>{props.categoryObj.name}</Text>
-          <Badge color="pink">{props.categoryObj.lastActivity}</Badge>
+          <Badge color="blue">{props.categoryObj.lastActivity}</Badge>
         </Group>
 
         <Text size="sm" c="dimmed" lineClamp={3}>
           {props.categoryObj.description}
         </Text>
         <Group>
-          <Button onClick={props.onEdit} color="yellow" variant="outline" mt="md" radius="md" w="45%">
+          <Button onClick={props.onEdit} color="blue" variant="filled" mt="md" radius="md" w="45%">
             Edit
           </Button>
           <Button
             onClick={() => props.onDelete(props.categoryObj.id)}
             color="red"
-            variant="outline"
+            variant="light"
             mt="md"
             radius="md"
             w="45%"
