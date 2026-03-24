eacf90e2d07200f01928784cd01aac50c8a3768c
Cesar <cessardevs@gmail.com>
2026-03-24 11:25:16 +0000

    refactor: card min width

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,36 +1,44 @@
 import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 
 const CategoryCard = (props) => {
-	return (
-		<div>
-			<Card shadow="sm" padding="lg" radius="md" withBorder maw={300}>
-				<Card.Section>
-					<Image
-						src={props.categoryObj.icon}
-						height={160}
-						alt="Category Image"
-					/>
-				</Card.Section>
+  return (
+    <div>
+      <Card shadow="sm" padding="lg" radius="md" withBorder maw={300}>
+        <Card.Section>
+          <Image
+            src={props.categoryObj.icon}
+            height={160}
+            miw={300} 
+            alt="Category Image"
+          />
+        </Card.Section>
 
-				<Group justify="space-between" mt="md" mb="xs">
-					<Text fw={500}>{props.categoryObj.name}</Text>
-					<Badge color="pink">{props.categoryObj.lastActivity}</Badge>
-				</Group>
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>{props.categoryObj.name}</Text>
+          <Badge color="pink">{props.categoryObj.lastActivity}</Badge>
+        </Group>
 
-				<Text size="sm" c="dimmed">
-					{props.categoryObj.description}
-				</Text>
-				<Group>
-					<Button color="yellow" variant="outline" mt="md" radius="md" w="45%">
-						Edit
-					</Button>
-					<Button onClick={() => props.onDelete(props.categoryObj.id)} color="red" variant="outline" mt="md" radius="md" w="45%">
-						Delete
-					</Button>
-				</Group>
-			</Card>
-		</div>
-	);
+        <Text size="sm" c="dimmed" lineClamp={3}>
+          {props.categoryObj.description}
+        </Text>
+        <Group>
+          <Button color="yellow" variant="outline" mt="md" radius="md" w="45%">
+            Edit
+          </Button>
+          <Button
+            onClick={() => props.onDelete(props.categoryObj.id)}
+            color="red"
+            variant="outline"
+            mt="md"
+            radius="md"
+            w="45%"
+          >
+            Delete
+          </Button>
+        </Group>
+      </Card>
+    </div>
+  );
 };
 
 export default CategoryCard;
