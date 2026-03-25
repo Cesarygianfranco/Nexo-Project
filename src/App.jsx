90118c404f02f6f6e6fd45d4286a9dc67c9056f5
Gianfranco <gianfranco.devita@hotmail.com>
2026-03-25 16:11:14 +0100

    feat: Create ValuationPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import HeaderSearch from "./components/Header/HeaderSearch";
 import ProductsList from "./pages/ProductsList";
 import BinPage from "./pages/BinPage";
 import EditProductPage from "./pages/EditProductPage";
+import ValuationPage from "./pages/ValuationPage";
 
 function App() {
 	return (
@@ -17,8 +18,12 @@ function App() {
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/products/:categoryId" element={<ProductsList />} />
-					<Route path="/productsDetails/:productId" element={<EditProductPage />} />
+					<Route
+						path="/productsDetails/:productId"
+						element={<EditProductPage />}
+					/>
 					<Route path="/bin" element={<BinPage />} />
+					<Route path="/valuation" element={<ValuationPage />} />
 				</Routes>
 			</MantineProvider>
 		</>
---
+++ src/pages/ValuationPage.jsx
@@ -0,0 +1,9 @@
+function ValuationPage() {
+	return (
+		<div>
+			<h1>Welcome to Valuation Page!</h1>
+		</div>
+	);
+}
+
+export default ValuationPage;
