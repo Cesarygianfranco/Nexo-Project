27e2f252406b6121fe8314f6a28132f57db0476f
Cesar <cessardevs@gmail.com>
2026-03-27 10:21:23 +0000

    refactor: changed form select options language

--- src/constants/categoryIcons.js
+++ src/constants/categoryIcons.js
@@ -1,14 +1,14 @@
 // src/constants/categoryIcons.js
 export const CATEGORY_ICONS = [
   { value: "/images/laptop.jpg", label: "Laptop" },
-  { value: "/images/discoduro.jpg", label: "Disco Duro" },
-  { value: "/images/memoriaram.jpg", label: "Memoria RAM" },
+  { value: "/images/discoduro.jpg", label: "Hard drives" },
+  { value: "/images/memoriaram.jpg", label: "Memory RAM" },
   { value: "/images/monitor.jpg", label: "Monitor" },
   { value: "/images/mouse.jpg", label: "Mouse" },
-  { value: "/images/packspc.jpg", label: "Ordenadores" },
-  { value: "/images/pc.jpg", label: "Torres" },
+  { value: "/images/packspc.jpg", label: "Builds" },
+  { value: "/images/pc.jpg", label: "PC" },
   { value: "/images/pendrives.jpg", label: "PenDrives" },
-  { value: "/images/procesador.jpg", label: "Procesadores" },
-  { value: "/images/tarjetagrafica.jpg", label: "Tarjetas Gráficas" },
-  { value: "/images/teclado.jpg", label: "Teclado" },
+  { value: "/images/procesador.jpg", label: "Processor" },
+  { value: "/images/tarjetagrafica.jpg", label: "Graphic Cards" },
+  { value: "/images/teclado.jpg", label: "Keyboard" },
 ];
